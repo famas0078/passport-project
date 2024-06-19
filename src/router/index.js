09738efd7866cc2da5827ad5f4518afe4add668e
Yason <nazardenr@mail.ru>
2024-06-19 18:54:04 +0300

    change route

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,4 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import OneSectionProjectPass from "@/pages/OneSection/OneSectionProjectPass.vue";
 import IndexSection from '../pages/index.vue'
 export default createRouter({
   history: createWebHistory(),
