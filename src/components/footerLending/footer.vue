ed7f5d88445c1265cb6ad0e6e68dab752b17733e
Maritxuak <97700561+Maritxuak@users.noreply.github.com>
2024-06-28 23:54:17 +0300

    Add Header and Footer on Lending

---
+++ src/components/footerLending/footer.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+  name: "footer"
+}
+</script>
+
+<template>
+  <div class="wrapper container">
+    <div class="row">
+      <div class="col-12 d-flex justify-content-between">
+        <div class="wrapper-left text-left col-xxl-6">
+          АО "Рантье"<br />
+          143025, Московская область, Сколково ИНН<br />
+          © 2024. Все права защищены.
+        </div>
+
+        <div class="wrapper-right text-right col-xxl-6">
+          <div class="wrapper-right-item">Пользовательское соглашение</div>
+          <div class="wrapper-right-item">Политика конфиденциальности</div>
+          <div class="wrapper-right-item">Публичная оферета</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.wrapper-right-item {
+  color: var(--color-purple);
+}
+</style>
--- src/components/headerLending/header.vue
+++ src/components/headerLending/header.vue
@@ -1,13 +1,16 @@
 <template>
-    <div class="wrapper">
+    <div class="wrapper wrapper-header-lending">
         <div class="container">
             <div class="row">
                 <div class="col-12 d-flex justify-content-between">
                     <div class="logotype">
-                        <img src="../../assets/img/logoLendingPage.svg" alt="">
+                        <img src="../../assets/img/logoLendingPage.svg" alt="Логотип Рантье">
+                        <div class="logotype-text">
+                            Развиваем города
+                        </div>
                     </div>
-                    <div class="btn">
-                        Заполнить анкету
+                    <div class="btn-costum">
+                        <router-link to="/passport" class="btn-costum-inner">Заполнить анкету</router-link>
                     </div>
                 </div>
             </div>
