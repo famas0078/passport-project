bae057803d493cd5981ce475caa345c819659272
Maritxuak <97700561+Maritxuak@users.noreply.github.com>
2024-06-29 01:12:08 +0300

    add-faq

---
+++ src/components/faq/index.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="wrapper-faq">
+        <div class="container">
+            <h1 class="title">
+                вопрос/ответ
+            </h1>
+        </div>
+    </div>
+</template>
--- src/pages/indexPages/indexLending.vue
+++ src/pages/indexPages/indexLending.vue
@@ -1,6 +1,7 @@
 <template>
     <Header />
     <Recommendations />
+    <Faq />
     <Footer />
 </template>
 
@@ -8,12 +9,14 @@
 import Recommendations from "@/components/recommendationsLending/recommendations.vue"
 import Header from "@/components/headerLending/header.vue"
 import Footer from "@/components/footerLending/footer.vue"
+import Faq from "@/components/faq/index.vue"
 export default {
     name: "indexLending",
     components:{
         Header,
         Footer,
         Recommendations,
+        Faq,
     }
 }
 </script>
