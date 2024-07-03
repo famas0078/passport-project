e0f4934888a340c12d9898bda9e9ff2551e3d062
Maritxuak <97700561+Maritxuak@users.noreply.github.com>
2024-07-04 00:16:50 +0300

    update

--- src/components/recommendationsLending/recommendations.vue
+++ src/components/recommendationsLending/recommendations.vue
@@ -1,85 +1,95 @@
 <template>
     <div class="wrapper-recommendations">
-        <!-- <div class="container">
+        <div class="container">
             <div class="row">
-                <div class="col-12 d-flex justify-content-between">
-                    <div class="col-xxl-6 item item-left d-flex justify-content-between">
-                        <div class=" left-container">
-                            <div class="title">
-                                <div class="big-title">
-                                    Создавайте личный (семейный) капитал
-                                </div>
-                                <div class="small-title">
-                                    Научитесь им управлять и приумножать. Достигайте свои финансовые цели
-                                </div>
+                <div class="col-xxl-6 item item-left d-flex justify-content-between">
+                    <div class="mobile_img">
+                        <img src="../../assets/img/Cloud.svg" alt="">
+                    </div>
+                    <div class=" left-container">
+                        <div class="title">
+                            <div class="big-title">
+                                Создавайте личный (семейный) капитал
                             </div>
-                            <div class="arrow">
-                                <img src="../../assets/img/ArrowRightRecommendations.svg" alt="Стрелка">
+                            <div class="small-title">
+                                Научитесь им управлять и приумножать. Достигайте свои финансовые цели
                             </div>
                         </div>
-                        <div class="">
-                            <img src="../../assets/img/Cloud.svg" alt="">
+                        <div class="arrow">
+                            <img src="../../assets/img/ArrowRightRecommendations.svg" alt="Стрелка">
                         </div>
                     </div>
-                    <div class="col-xxl-6 item d-flex justify-content-between">
-                        <div class=" left-container">
-                            <div class="title">
-                                <div class="big-title">
-                                    Создавайте личный (семейный) капитал
-                                </div>
-                                <div class="small-title">
-                                    Научитесь им управлять и приумножать. Достигайте свои финансовые цели
-                                </div>
-                            </div>
-                            <div class="arrow">
-                                <img src="../../assets/img/ArrowRightRecommendations.svg" alt="Стрелка">
-                            </div>
-                        </div>
-                        <div class="">
-                            <img src="../../assets/img/Cloud.svg" alt="">
-                        </div>
+                    <div class="dekstop_img">
+                        <img src="../../assets/img/Cloud.svg" alt="">
                     </div>
                 </div>
-                <div class="col-12 d-flex justify-content-between">
-                    <div class="col-xxl-6 item item-left d-flex justify-content-between">
-                        <div class=" left-container">
-                            <div class="title">
-                                <div class="big-title">
-                                    Создавайте личный (семейный) капитал
-                                </div>
-                                <div class="small-title">
-                                    Научитесь им управлять и приумножать. Достигайте свои финансовые цели
-                                </div>
+                <div class="col-xxl-6 item d-flex justify-content-between">
+                    <div class="mobile_img">
+                        <img src="../../assets/img/Cloud.svg" alt="">
+                    </div>
+                    <div class=" left-container">
+                        <div class="title">
+                            <div class="big-title">
+                                Создавайте личный (семейный) капитал
                             </div>
-                            <div class="arrow">
-                                <img src="../../assets/img/ArrowRightRecommendations.svg" alt="Стрелка">
+                            <div class="small-title">
+                                Научитесь им управлять и приумножать. Достигайте свои финансовые цели
                             </div>
                         </div>
-                        <div class="">
-                            <img src="../../assets/img/Cloud.svg" alt="">
+                        <div class="arrow">
+                            <img src="../../assets/img/ArrowRightRecommendations.svg" alt="Стрелка">
                         </div>
                     </div>
-                    <div class="col-xxl-6 item d-flex justify-content-between">
-                        <div class=" left-container">
-                            <div class="title">
-                                <div class="big-title">
-                                    Создавайте личный (семейный) капитал
-                                </div>
-                                <div class="small-title">
-                                    Научитесь им управлять и приумножать. Достигайте свои финансовые цели
-                                </div>
-                            </div>
-                            <div class="arrow">
-                                <img src="../../assets/img/ArrowRightRecommendations.svg" alt="Стрелка">
-                            </div>
-                        </div>
-                        <div class="">
-                            <img src="../../assets/img/Cloud.svg" alt="">
-                        </div>
+                    <div class="dekstop_img">
+                        <img src="../../assets/img/Cloud.svg" alt="">
                     </div>
                 </div>
             </div>
-        </div> -->
+            <div class="row">
+                <div class="col-xxl-6 item item-left d-flex justify-content-between">
+                    <div class="mobile_img">
+                        <img src="../../assets/img/Cloud.svg" alt="">
+                    </div>
+                    <div class=" left-container">
+                        <div class="title">
+                            <div class="big-title">
+                                Создавайте личный (семейный) капитал
+                            </div>
+                            <div class="small-title">
+                                Научитесь им управлять и приумножать. Достигайте свои финансовые цели
+                            </div>
+                        </div>
+                        <div class="arrow">
+                            <img src="../../assets/img/ArrowRightRecommendations.svg" alt="Стрелка">
+                        </div>
+                    </div>
+                    <div class="dekstop_img">
+                        <img src="../../assets/img/Cloud.svg" alt="">
+                    </div>
+                </div>
+                <div class="col-xxl-6 item d-flex justify-content-between">
+                    <div class="mobile_img">
+                        <img src="../../assets/img/Cloud.svg" alt="">
+                    </div>
+                    <div class=" left-container">
+                        <div class="title">
+                            <div class="big-title">
+                                Создавайте личный (семейный) капитал
+                            </div>
+                            <div class="small-title">
+                                Научитесь им управлять и приумножать. Достигайте свои финансовые цели
+                            </div>
+                        </div>
+                        <div class="arrow">
+                            <img src="../../assets/img/ArrowRightRecommendations.svg" alt="Стрелка">
+                        </div>
+                    </div>
+                    <div class="dekstop_img">
+                        <img src="../../assets/img/Cloud.svg" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
