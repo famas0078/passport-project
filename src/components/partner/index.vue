aa350b6ba92926594f7c92e9fd7c3fbfe7a653ce
Maritxuak <97700561+Maritxuak@users.noreply.github.com>
2024-07-03 18:35:54 +0300

    update

--- src/components/partner/index.vue
+++ src/components/partner/index.vue
@@ -1,38 +1,81 @@
 <template>
-<div class="wrapper-partner">
-    <div class="container">
-        <div class="title">
-
-        </div>
-        <div class="row">
-            <div class="col-xxl-6">
-                <div class="title">
-                    <div class="big-title">
-                        1. Зарегистрируйтесь на платформе
-                    </div>
-                    <div class="small-title">
-                        Просто заполните необходимую информацию, создайте учетную запись и подтвердите свою почту.
-                    </div>
-                    <div class="time">
-                        3 минуты
+    <div class="wrapper-partner">
+        <div class="container">
+            <div class="title">
+                Как присоединиться к партнерской программе и зарабатывать на активности своих рефералов?
+            </div>
+            <div class="row mt-5">
+                <div class="col-xl-6 col-lg-6 col-md-12 col-12">
+                    <div class="card-item d-flex">
+                        <div class="card-text">
+                            <h3 class="title-card-register">
+                                1. Зарегистрируйтесь на платформе
+                            </h3>
+                            <div class="des-card flex-grow-1">
+                                Просто заполните необходимую информацию, создайте учетную запись и подтвердите свою почту.
+                            </div>
+                            <div class="time">
+                                <div class="time_inner">
+                                    3 минуты
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
-                <div class="time">
-
+                <div class="col-xl-6 col-lg-6 col-md-12 col-12">
+                    <div class="card-item d-flex">
+                        <div class="card-text">
+                            <h3 class="title-card-register">
+                                2. Возьмите в личном кабинете реферальную ссылку
+                            </h3>
+                            <div class="des-card flex-grow-1">
+                                Найдите свою реферальную ссылку в личном кабинете
+                            </div>
+                            <div class="time">
+                                <div class="time_inner">
+                                    10 секунд
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
-            <div class="col-xxl-6">
-                
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-xxl-6">
-
-            </div>
-            <div class="col-xxl-6">
-                
+            <div class="row mt-4">
+                <div class="col-xl-6 col-lg-6 col-md-12 col-12">
+                    <div class="card-item d-flex flex-column h-100">
+                        <div class="card-text flex-grow-1">
+                            <h3 class="title-card-register">
+                                3. Делитесь своей реферальной ссылкой со своими друзьями и подписчикамиу
+                            </h3>
+                            <div class="des-card flex-grow-1">
+                                Поделитесь ссылкой с друзьями и подписчиками через соцсети
+                            </div>
+                        </div>
+                        <div class="time">
+                            <div class="time_inner">
+                                5 минут
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-6 col-lg-6 col-md-12 col-12">
+                    <div class="card-item d-flex flex-column h-100">
+                        <div class="card-text flex-grow-1">
+                            <h3 class="title-card-register">
+                                4. Получайте вознаграждение
+                            </h3>
+                            <div class="des-card flex-grow-1">
+                                После регистрации рефералов и их активности, вы начнете получать вознаграждение
+                            </div>
+                        </div>
+                        <div class="time">
+                            <div class="time_inner">
+                                24 часа
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
-</div>
 </template>
