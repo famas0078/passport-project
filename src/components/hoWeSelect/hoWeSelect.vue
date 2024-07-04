1a6d155bca3f9e94453e89e66e4b390bc4fe5286
Maritxuak <97700561+Maritxuak@users.noreply.github.com>
2024-07-04 14:15:50 +0300

    add-adaptive

--- src/components/hoWeSelect/hoWeSelect.vue
+++ src/components/hoWeSelect/hoWeSelect.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="wrapper-how-we-selected justify-content-between container">
+        <img src="../../assets/img/otbor.svg" class="mobile_img" alt="" width="280px" height="202px">
         <div class="wrapper-text">
             <h2 class="title">
                 Как мы отбираем проекты,<br/>
@@ -9,6 +10,6 @@
                 «Рантье. Развиваем города» – это платформа коллективного финансирования в проекты, которые помогают городам становится умными, креативными и процветающими.
             </div>
         </div>
-        <img src="../../assets/img/otbor.svg" alt="">
+        <img src="../../assets/img/otbor.svg" class="dekstop_img" alt="">
     </div>
 </template>
