cbccab6f17c48c4543125fe9f4ec94c9aaf2ee2e
Maritxuak <97700561+Maritxuak@users.noreply.github.com>
2024-07-04 13:38:11 +0300

    update oneSection, recommendations

--- src/components/descriprionOneSection/index.vue
+++ src/components/descriprionOneSection/index.vue
@@ -1,23 +1,31 @@
 <template>
     <div class="wrapper-one-section">
         <div class="container">
-            <div class="row d-flex">
-                <div class="title">
+            <div class="row">
+                <div class="title col-xl-6 mt-4 col-lg-12 col-md-12 col-12">
                     <div class="big-title">
-                        Инвестируйте в свой город
+                        <span class="big-title-inner">
+                            Инвестируйте<br>
+                        </span> в свой город
                     </div>
                     <div class="small-title">
-                        Начните вести финансово здоровый образ жизни, помогая городам стать комфортными, креативными и процветающими.
+                        Начните вести финансово здоровый образ жизни, помогая городам стать комфортными, креативными и
+                        процветающими.
                     </div>
-                    <div class="wrapper-btn">
-                        <div class="btn-cstm">Присоединиться к сообществу</div>
-                        <div class="btn-cstm-cabinet">Создать личный кабинет</div>
+                    <div class="group-btn">
+                        <div class="btn-to-register d-flex justify-content-center">
+                            <router-link to="" class="btn-a">Присоединиться к сообществу</router-link>
+                        </div>
+                        <div class="btn-to-invest d-flex justify-content-center">
+                            <router-link to="" class="btn-a">Создать личный кабинет</router-link>
+                        </div>
                     </div>
                 </div>
-                <div class="image">
+                <div class="image dekstop-image col-xl-6 mt-4 col-lg-6 col-md-12 col-12">
                     <img src="../../assets/img/OneSection.svg" alt="">
                 </div>
             </div>
         </div>
+
     </div>
 </template>
