e564bde9ed18416d8c5deea2bedf4e953aaa7499
Maritxuak <97700561+Maritxuak@users.noreply.github.com>
2024-10-07 18:18:17 +0300

    add indexInvestment.vue, header

---
+++ src/components/headerInvestment/header.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  name: "header"
+}
+</script>
+
+<template>
+1
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/pages/indexPages/indexInvestment.vue
@@ -0,0 +1,32 @@
+
+<template>
+  <div class="d-flex flex-column h-100">
+    <Header />
+
+    <Footer/>
+  </div>
+</template>
+
+<script>
+import Header from "@/components/header/header.vue";
+import Footer from "@/components/footer/footer.vue";
+
+export default {
+  name: "indexInvestment",
+  components: {
+    Footer,
+    Header
+  },
+
+  data(){
+    return{
+
+    }
+  },
+
+}
+</script>
+
+<style scoped>
+
+</style>
