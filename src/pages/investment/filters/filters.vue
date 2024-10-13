889f072b8ca7b39d8f8f365572ace5337abdf932
Maritxuak <97700561+Maritxuak@users.noreply.github.com>
2024-10-13 18:55:56 +0300

    Updating filters.vue

--- src/pages/investment/filters/filters.vue
+++ src/pages/investment/filters/filters.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="wrapper-filters">
     <Header :headerActiveMenu= this.headerActiveMenu />
-    <div class="main-filters container mt-5">
+    <div class="main-filters mt-5">
       <div class="title-filters text-uppercase">
         Проекты
       </div>
@@ -22,6 +22,42 @@
               :class="{ 'menu-item-active': statusProject === 3 }"
               @click="updateStatusProjectFilter(3)"
           >Сбор завершен</div>
+        </div>
+      </div>
+      <div class="main-projects mt-3 d-flex justify-content-between">
+        <div class="main-projects-left">
+          <div class="search-projects-left">
+            <input type="text" class="input-search-item" placeholder="Поиск по названию проекта">
+            <span class="search-icon"><img src="../../../assets/img/investmentPages/Seacrh.svg" alt=""></span>
+          </div>
+          <div class="rating-projects-left">
+
+          </div>
+        </div>
+        <div class="main-projects-right">
+          <div class="projects-item d-flex">
+            <div class="projects-item-img">
+              <img src="../../../assets/img/investmentPages/noImageProject.png" alt="">
+            </div>
+            <div class="projects-item-description">
+              <div class="description-category">
+                Категория
+              </div>
+              <div class="project-name">
+                Название проекта
+              </div>
+              <div class="description-text">
+                Вот вам яркий пример современных тенденций — синтетическое
+                тестирование позволяет выполнить важные задания по разработке
+                прогресса профессионального сообщества.
+              </div>
+              <div class="description-under">
+                <div class="project-rating"></div>
+                <div class="project-popular"></div>
+                <div class="project-data"></div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
